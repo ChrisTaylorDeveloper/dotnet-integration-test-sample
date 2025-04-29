73414c0446c65e8cd9b8aa4a482aa8f6310107b2
Chris Taylor <chris@christaylordeveloper.co.uk>
2025-04-29 19:29:00 +0100

    Remove fullstop from output.

--- EFGetStarted/Program.cs
+++ EFGetStarted/Program.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using var db = new BloggingContext();
 
 // Note: This sample requires the database to be created before running.
-Console.WriteLine($"Database path: {db.DbPath}.");
+Console.WriteLine($"Database path: {db.DbPath}");
 
 // Create
 Console.WriteLine("Inserting a new blog");
